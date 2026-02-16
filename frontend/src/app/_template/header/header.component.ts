2898e7874014844fd882e0453ab238ab460cdbb9
Mitch1802 <77403827+Mitch1802@users.noreply.github.com>
2026-02-16 13:16:22 +0100

    fix(ui): fix oninit error

--- frontend/src/app/_template/header/header.component.ts
+++ frontend/src/app/_template/header/header.component.ts
@@ -23,7 +23,7 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
         AsyncPipe
     ]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   globalDataService = inject(GlobalDataService);
   private router = inject(Router);
 
